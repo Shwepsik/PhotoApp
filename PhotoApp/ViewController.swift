c4769ac84c9b55d23a7dead8f7fe9c14693de1f3
Valerii <www.l1nk32@gmail.com>
2019-05-29 02:13:58 +0300

    Initial Commit

---
+++ PhotoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by Valerii on 29.05.2019.
+//  Copyright © 2019 Valerii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
